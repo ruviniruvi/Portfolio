db1861710b1e0a1f153ef86ae462d42c553c0f68
ruviniruvi <ruviniruvi@gmail.com>
2022-03-11 13:13:17 -0500

    Added scroll up feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,25 +54,38 @@ const skillsContent = document.getElementsByClassName('skills_content'),
     skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
     })
-/*==================== QUALIFICATION TABS ====================*/
 
-
-/*==================== SERVICES MODAL ====================*/
-
-
-/*==================== PORTFOLIO SWIPER  ====================*/
-
-
-/*==================== TESTIMONIAL ====================*/
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+const sections = document.querySelectorAll('section[id]')
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 
 /*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*==================== DARK LIGHT THEME ====================*/ 
